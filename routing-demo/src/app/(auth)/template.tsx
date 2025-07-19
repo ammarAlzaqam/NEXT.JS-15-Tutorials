9734616dbe72b5a53747e1b6994c6f3d936e674b
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-19 12:14:41 +0300

    template

--- routing-demo/src/app/(auth)/template.tsx
+++ routing-demo/src/app/(auth)/template.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 type NavLink = {
   name: string;
@@ -27,12 +27,28 @@ const navLinks: NavLink[] = [
 export default function AuthLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
   const authPath = pathname.split("/").pop();
+  const [input, setInput] = useState("");
   return (
     <section>
+      <div className="p-2">
+        <input
+          type="text"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
+          className="bg-white text-black"
+        />
+      </div>
       <ul className="flex justify-center items-center gap-5">
         {navLinks.map(({ name, href }) => (
           <li key={name}>
-            <Link href={href} className={`${authPath === href.slice(1) && "text-green-500 transition"}`}>{name}</Link>
+            <Link
+              href={href}
+              className={`${
+                authPath === href.slice(1) && "text-green-500 transition"
+              }`}
+            >
+              {name}
+            </Link>
           </li>
         ))}
       </ul>

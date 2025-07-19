f5ff45f4466df50cd60a74880aa30458ab77bd6c
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-19 07:12:33 +0300

    layouts

---
+++ routing-demo/src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <div className="p-5 text-center bg-yellow-500 text-white text-xl">
+      Footer
+    </div>
+  );
+}
---
+++ routing-demo/src/components/header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <div className="p-5 text-center bg-amber-500 text-white text-xl">Header</div>
+  )
+}

22a91b14ab397b301fb581c9fb2c25eed431bbf7
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-19 12:56:33 +0300

    recovering from errors

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,9 +1,28 @@
 "use client";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  const reload = () => {
+    startTransition(() => {
+      router.refresh();
+      reset();
+    });
+  };
+
   return (
-    <div className="flex justify-center items-center text-red-500">
-      {error.message}
+    <div className="flex flex-col justify-center items-center gap-3">
+      <p className="text-red-500">{error.message}</p>
+      <button onClick={reload}>Try again</button>
     </div>
   );
 }

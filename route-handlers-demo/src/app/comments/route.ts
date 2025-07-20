c9f866ed1fbf9902bf2cf31199cd5adeaf102a08
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-20 09:25:16 +0300

    handling POST requests

--- route-handlers-demo/src/app/comments/route.ts
+++ route-handlers-demo/src/app/comments/route.ts
@@ -1,5 +1,27 @@
+import { NextRequest } from "next/server";
 import { comments } from "./data";
 
 export async function GET() {
-    return Response.json({comments})
+  return Response.json({ comments });
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const newComment = { id: comments.length + 1, ...body };
+  comments.push(newComment);
+
+  //return Response.json(
+  //  { message: "Comment created successfully" },
+  //  { status: 201 }
+  //);
+  return new Response(
+    JSON.stringify({
+      message: "Comment created successfully",
+      ...newComment,
+    }),
+    {
+      headers: { "Content-Type": "application/json" },
+      status: 201,
+    }
+  );
 }

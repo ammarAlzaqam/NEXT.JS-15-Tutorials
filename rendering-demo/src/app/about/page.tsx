49d83d052e1e5657240945bca8d2f53f8659fb41
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-20 21:22:56 +0300

    dynamic rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,4 +1,8 @@
-export default function AboutPage() {
+import { cookies } from "next/headers";
+
+export default async function AboutPage() {
   console.log("About server component");
+  const cookieStore = await cookies();
+  console.log(cookieStore.get("theme"));
   return <h1>About page {new Date().toLocaleTimeString()}</h1>;
 }

64349b9dd68975a2b725fae5a533e9744021d2a2
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-21 12:56:33 +0300

    context providers

--- rendering-demo/src/app/client-route/page.tsx
+++ rendering-demo/src/app/client-route/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import ImageSlider from "@/components/imageSlider";
+// import ImageSlider from "@/components/imageSlider";
+import { useTheme } from "@/components/theme-provider";
 
 // import { serverSideFunction } from "@/utils/server-utils";
 
 export default function ClientRoutePage() {
   //   const result = serverSideFunction();
-  const settings = {
-    dots: true,
-  };
-  return <ImageSlider />;
+  const theme = useTheme();
+  return <h1 style={{ color: theme.colors.secondary }}>Client route page</h1>;
 }
